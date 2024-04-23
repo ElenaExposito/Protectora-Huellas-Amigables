d9456d3c3da64592d70d924e056f54e58ed5a827
Elena Expósito <elexla97@gmail.com>
2024-04-23 17:16:21 +0200

    Nav mobile view WIP - Open + Close  menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,54 +3,61 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <nav className="navbar">
-        <div className="menuBasic">
-          <h1 className="menuTitle">Menú</h1>
+      <nav id="nav" className="navbar open">
+        <div className="menuBasic open">
+          <h1 className="menuTitle open">Menú</h1>
           <img
+            id="open"
             className="burguerMenu"
             src="./src/assets/Burguer icon.png"
-            alt="Icono Menú"
+            alt="Abrir el Menú"
+          />
+          <img
+            id="close"
+            className="closedMenu"
+            src="./src/assets/Close icon.png"
+            alt="Cerrar el Menú"
           />
         </div>
         <ul className="navList">
-          <hr className="menuLineStyle zero"></hr>
-          <li className="navListItem one">
+          <hr className="menuLineStyle open zero"></hr>
+          <li className="navListItem open one">
             <a href="#inicio">Inicio</a>
           </li>
-          <hr className="menuLineStyle two"></hr>
-          <li className="navListItem three">
+          <hr className="menuLineStyle open two"></hr>
+          <li className="navListItem open three">
             <a href="#animales">Animales en Adopción</a>
           </li>
-          <hr className="menuLineStyle four"></hr>
-          <li className="navListItem five">
+          <hr className="menuLineStyle open four"></hr>
+          <li className="navListItem open five">
             <a href="#colabora">Colabora</a>
           </li>
-          <hr className="menuLineStyle six"></hr>
+          <hr className="menuLineStyle open six"></hr>
           <li className="seven">
             <img
-              className="navLogo"
+              className="navLogo open"
               src="./src/assets/Huellas Amigables.png"
               alt="Logotipo Huellas Amigables"
             />
           </li>
-          <hr className="menuLineStyle height"></hr>
-          <li className="navListItem nine">
+          <hr className="menuLineStyle open height"></hr>
+          <li className="navListItem open nine">
             <a href="#nosotros">Quiénes somos</a>
           </li>
-          <hr className="menuLineStyle ten"></hr>
-          <li className="navListItem eleven">
+          <hr className="menuLineStyle open ten"></hr>
+          <li className="navListItem open eleven">
             <a href="#area-privada">Área privada</a>
           </li>
-          <hr className="menuLineStyle twelve"></hr>
-          <li className="navListItem thirteen">
+          <hr className="menuLineStyle open twelve"></hr>
+          <li className="navListItem open thirteen">
             <a href="#contacto">Contacto</a>
           </li>
         </ul>
       </nav>
-      <div className="footprintsLine">
+      <div className="footprintsLine open">
         <div className="imageFootprintsLine"></div>
-        <h1 className="logoName">Huellas Amigables</h1>
-        <div className="imageFootprintsLine"></div>
+        <h1 className="logoName open">Huellas Amigables</h1>
+        <div className="imageFootprintsLine open"></div>
       </div>
     </header>
   );
