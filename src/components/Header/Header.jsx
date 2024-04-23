5e01edcb19182099cee9df4b8a6a935494db4f3a
Elena Expósito <elexla97@gmail.com>
2024-04-23 14:00:53 +0200

    Nav mobile view WIP

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,30 +4,45 @@ function Header() {
   return (
     <header className="header">
       <nav>
+        <div className="menuBasic">
+          <h1 className="menuTitle">Menú</h1>
+          <img
+            className="burguerMenu"
+            src="./src/assets/Burguer icon.png"
+            alt="Icono Menú"
+          />
+        </div>
         <ul className="navList">
-          <li className="navListItem">
+          <hr className="menuLineStyle zero"></hr>
+          <li className="navListItem one">
             <a href="#inicio">Inicio</a>
           </li>
-          <li className="navListItem">
+          <hr className="menuLineStyle two"></hr>
+          <li className="navListItem three">
             <a href="#animales">Animales en Adopción</a>
           </li>
-          <li className="navListItem">
+          <hr className="menuLineStyle four"></hr>
+          <li className="navListItem five">
             <a href="#colabora">Colabora</a>
           </li>
-          <li>
+          <hr className="menuLineStyle six"></hr>
+          <li className="seven">
             <img
               className="navLogo"
               src="./src/assets/Huellas Amigables.png"
               alt="Logotipo Huellas Amigables"
             />
           </li>
-          <li className="navListItem">
+          <hr className="menuLineStyle height"></hr>
+          <li className="navListItem nine">
             <a href="#nosotros">Quiénes somos</a>
           </li>
-          <li className="navListItem">
+          <hr className="menuLineStyle ten"></hr>
+          <li className="navListItem eleven">
             <a href="#area-privada">Área privada</a>
           </li>
-          <li className="navListItem">
+          <hr className="menuLineStyle twelve"></hr>
+          <li className="navListItem thirteen">
             <a href="#contacto">Contacto</a>
           </li>
         </ul>
